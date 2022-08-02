db81b32ec16eddc92702b63343178d6824fa9c4e
baibaixy <2942583916@qq.com>
2022-08-02 14:58:56 +0800

    sidebar样式完成

---
+++ src/permission.js
@@ -0,0 +1,18 @@
+import router from '@/router'
+import store from '@/store'
+
+const whiteList = ['/login', '/404']
+router.beforeEach((to, from, next) => {
+  const token = store.state.user.token
+  if (token) {
+    if (to.path === '/login') return next('/')
+    next()
+  } else {
+    const isClude = whiteList.includes(to.path)
+    if (isClude) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
+})
