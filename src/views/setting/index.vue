0467d6895cd4239d0fe9b256099d9458a81cd610
baibaixy <2942583916@qq.com>
2022-08-11 11:48:18 +0800

    完成角色部分功能

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,20 +1,114 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>公司设置</h2>
+      <el-tabs v-model="activeName">
+        <el-tab-pane label="角色管理" name="first">
+          <el-button type="primary" @click="dialogVisible = true"
+            >新增角色</el-button
+          >
+          <!-- 表格 -->
+          <el-table :data="tableData" style="width: 100%">
+            <el-table-column type="index" label="序号"> </el-table-column>
+            <el-table-column prop="name" label="角色"> </el-table-column>
+            <el-table-column prop="description" label="描述"> </el-table-column>
+            <el-table-column prop="address" label="操作">
+              <template>
+                <el-button size="small" type="success">分配权限</el-button>
+                <el-button size="small" type="primary">编辑</el-button>
+                <el-button size="small" type="danger">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+          <!-- 分页 -->
+          <el-pagination
+            layout="prev, pager, next, sizes"
+            :page-size="pagesize"
+            :total="total"
+            @current-change="currentChange"
+            @size-change="handleSizeChange"
+            :page-sizes="[5, 8, 10, 20]"
+          >
+          </el-pagination>
+        </el-tab-pane>
+        <el-tab-pane label="公司信息" name="second">配置管理</el-tab-pane>
+      </el-tabs>
     </div>
+    <el-dialog
+      @close="closeDialog"
+      title="新增角色"
+      :visible.sync="dialogVisible"
+      width="50%"
+    >
+      <el-form ref="form" :model="form" label-width="80px" :rules="formRules">
+        <el-form-item label="角色名称" prop="name">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="角色描述" prop="description">
+          <el-input v-model="form.description"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="onClose">取 消</el-button>
+        <el-button type="primary" @click="onConfirm">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import { getRolesApi, addRolesApi } from '@/api/role'
 export default {
   data() {
-    return {}
+    return {
+      activeName: 'first',
+      tableData: [],
+      total: 0,
+      pagesize: 3,
+      page: 1,
+      dialogVisible: false,
+      form: { name: '', description: '' },
+      formRules: {
+        name: [{ required: true, message: '请输入角色名称', trigger: 'blur' }]
+      }
+    }
   },
 
-  created() {},
+  created() {
+    this.getRolesApi()
+  },
 
-  methods: {}
+  methods: {
+    async getRolesApi() {
+      const { rows, total } = await getRolesApi({
+        page: this.page,
+        pagesize: this.pagesize
+      })
+      this.tableData = rows
+      this.total = total
+    },
+    currentChange(val) {
+      this.page = val
+      this.getRolesApi()
+    },
+    handleSizeChange(val) {
+      this.pagesize = val
+      this.getRolesApi()
+    },
+    onClose() {
+      this.dialogVisible = false
+    },
+    async onConfirm() {
+      await this.$refs.form.validate()
+      await addRolesApi(this.form)
+      this.$message.success('添加成功')
+      this.dialogVisible = false
+      this.getRolesApi()
+    },
+    closeDialog() {
+      this.$refs.form.restFields()
+      this.form.description = ''
+    }
+  }
 }
 </script>
 
